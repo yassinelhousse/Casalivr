0169d98f2d372be8a4fa460af9088fa350fbbd03
MOKIR112 <lakzazmhraz@gmail.com>
2026-02-16 15:18:47 +0000

    delete role logic for now

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -5,28 +5,16 @@ export const createOrder = async (req, res) => {
     const order = await orderService.createOrder(req.user.id, req.body.items);
 
     res.status(201).json(order);
-  } catch (e) {
-    res.status(400).json({ message: e.message });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 };
 
-export const myOrders = async (req, res) => {
+export const getMyOrders = async (req, res) => {
   try {
     const orders = await orderService.getMyOrders(req.user.id);
     res.json(orders);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
-};
-
-export const updateStatus = async (req, res) => {
-  try {
-    const { status } = req.body;
-
-    const order = await orderService.updateOrderStatus(req.params.id, status);
-
-    res.json(order);
-  } catch (e) {
-    res.status(400).json({ message: e.message });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
--- backend/src/services/order.service.js
+++ backend/src/services/order.service.js
@@ -1,16 +1,13 @@
-import { Order, OrderItem, MenuItem, Courier } from "../models/index.js";
+import { Order, OrderItem } from "../models/index.js";
 
 export const createOrder = async (userId, items) => {
   if (!items || items.length === 0) {
     throw new Error("Cart is empty");
   }
 
-  // calculate total
-  let deliveryFee = 10; 
-
   const order = await Order.create({
     userId,
-    deliveryFee,
+    deliveryFee: 10,
   });
 
   for (const item of items) {
@@ -30,24 +27,4 @@ export const getMyOrders = async (userId) => {
     include: [OrderItem],
     order: [["createdAt", "DESC"]],
   });
-};
-
-export const updateOrderStatus = async (orderId, status) => {
-  const order = await Order.findByPk(orderId);
-  if (!order) throw new Error("Order not found");
-
-  order.status = status;
-
-  // Fake courier assignment when validated
-  if (status === "VALIDEE") {
-    const couriers = await Courier.findAll();
-    if (couriers.length > 0) {
-      const random = couriers[Math.floor(Math.random() * couriers.length)];
-      order.courierId = random.id;
-    }
-  }
-
-  await order.save();
-
-  return order;
 };
