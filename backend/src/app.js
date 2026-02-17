e24d33d954cb57eb3277510d055eff006bbca7ec
MOKIR112 <lakzazmhraz@gmail.com>
2026-02-17 01:11:48 +0000

    creat test for auth and it passsed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import apiRoutes from "./routes/index.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", apiRoutes);
 
 export default app;
