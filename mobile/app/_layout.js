cac24c53842146e44f28a2d5910ab9377f91ef01
MOKIR112 <lakzazmhraz@gmail.com>
2026-02-20 02:15:54 +0000

    set upp zustand and asyncstorage

--- mobile/app/_layout.js
+++ mobile/app/_layout.js
@@ -1,16 +1,16 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect } from "react";
-// import { useAuthStore } from "../src/store/auth.store";
+import { useAuthStore } from "../src/store/auth.store";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  // const loadStoredAuth = useAuthStore((s) => s.loadStoredAuth);
+  const loadStoredAuth = useAuthStore((s) => s.loadStoredAuth);
 
-  // useEffect(() => {
-  //   loadStoredAuth();
-  // }, []);
+  useEffect(() => {
+    loadStoredAuth();
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
